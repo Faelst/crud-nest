6457bf37783659e01e6adade093334ffa7574c31
Rafael Silverio <fael_st@hotmail.com>
2022-01-18 01:56:15 -0300

    fist commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserDocument } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  create(createUserDto: CreateUserDto) {
+    const user = new this.userModel(createUserDto);
+    return user.save();
+  }
+
+  findAll() {
+    return this.userModel.find().exec();
+  }
+
+  findOne(id: string) {
+    return this.userModel.findOne({ _id: id }).exec();
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.userModel.findByIdAndUpdate({ _id: id }, updateUserDto).exec();
+  }
+
+  remove(id: string) {
+    return this.userModel.findByIdAndRemove({ _id: id }).exec();
+  }
+}
